f2068e0924fc9b189ca09f9d34d4b79cf3819a20
wxl <1695607239@qq.com>
2023-08-30 15:22:33 +0800

    待完成消息的发送，接受，删除，查询。

--- campus-alumni-matching-frontend/src/plugins/myAxios.js
+++ campus-alumni-matching-frontend/src/plugins/myAxios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const myAxios = axios.create({
-    baseURL : 'http://localhost:8888/api/'
+    baseURL : isDev ? 'http://localhost:8888/api/' : '线上环境';
 })
 
 myAxios.defaults.withCredentials = true;//设置为ture
